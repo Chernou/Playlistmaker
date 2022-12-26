4b58fa155065c8cbdca200110bd940c6566639f1
Chernou <chernov.i.u@gmail.com>
2022-12-25 19:15:43 -0500

    Домашняя работа №1

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.main_search)
+        val mediaButton = findViewById<Button>(R.id.main_media)
+        val settingsButton = findViewById<Button>(R.id.main_settings)
+
+        settingsButton.setOnClickListener {
+            val searchIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaButton.setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        searchButton.setOnClickListener {
+            val settingsIntent = Intent(this, SearchActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+
     }
 }
