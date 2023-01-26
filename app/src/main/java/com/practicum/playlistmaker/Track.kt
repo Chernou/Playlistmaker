518a0d10e70f3b14c01e3ed4551bb0207d32cc84
Chernou <chernov.i.u@gmail.com>
2023-01-26 07:35:16 -0500

    Домашняя работа №3

---
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -0,0 +1,37 @@
+package com.practicum.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+
+data class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUri: String
+)
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val trackName: TextView = itemView.findViewById(R.id.track_name)
+    private val artistName: TextView = itemView.findViewById(R.id.artist_name)
+    private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+    private val artwork: ImageView = itemView.findViewById(R.id.album_artwork)
+
+    fun bind(model: Track) {
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+        Glide.with(artwork)
+            .load(model.artworkUri)
+            .fitCenter()
+            .centerCrop()
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(2)))
+            .placeholder(R.drawable.ic_track_placeholder)
+            .into(artwork)
+    }
+}
