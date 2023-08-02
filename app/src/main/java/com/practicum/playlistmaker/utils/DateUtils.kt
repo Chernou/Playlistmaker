1c815291724db1d144aabee202359bcbca588bbf
Ilya Chernov <chernov.i.u@gmail.com>
2023-08-02 19:48:29 +0300

    21th sprint with reviewer's points

--- app/src/main/java/com/practicum/playlistmaker/utils/DateUtils.kt
+++ app/src/main/java/com/practicum/playlistmaker/utils/DateUtils.kt
@@ -10,15 +10,11 @@ object DateUtils {
     fun formatTime(trackTimeMillis: Int?): String = SimpleDateFormat("mm:ss", Locale.getDefault())
         .format(trackTimeMillis) ?: ""
 
-    fun getYear(str: String?): String? {
-        return if (str == null) {
-            null
-        } else {
-            val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()).parse(str)
-            val calendar = Calendar.getInstance(TimeZone.getDefault()).apply {
-                time = date
-            }
-            calendar.get(Calendar.YEAR).toString()
+    fun getYear(str: String?): String? = str?.let {
+        val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()).parse(str)
+        val calendar = Calendar.getInstance(TimeZone.getDefault()).apply {
+            time = date
         }
+        calendar.get(Calendar.YEAR).toString()
     }
 }
